ff782426cb545de0f33a67c450549ed58e210c54
ramroum <ramroum@192.168.92.217>
2014-11-21 16:19:31 +0100

    Signed-off-by: ramroum <ramroum@192.168.92.217>

---
+++ ci_projects/src/main/java/tn/edu/esprit/teamwork/App.java
@@ -0,0 +1,13 @@
+package tn.edu.esprit.teamwork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
